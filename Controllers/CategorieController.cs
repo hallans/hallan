ad2a69aeec4d9b129ee044793eeb98a2de4f785e
hallans <hallansouza54@gmail.com>
2017-05-26 20:42:05 -0300

    Add files via upload

---
+++ Controllers/CategorieController.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    public class CategorieController : Controller
+    {
+
+        private IList<Categorie> ListaCategoria = new List<Categorie>()
+        {
+            new Categorie() {Id=01, Name="Cobaia01" },
+            new Categorie() {Id=02, Name="Cobaia02" },
+            new Categorie() {Id=03, Name="Cobaia03" },
+            new Categorie() {Id=04, Name="Cobaia04" },
+            new Categorie() {Id=05, Name="Cobaia05" },
+
+        };
+
+
+        // GET: Categoria
+        public ActionResult Index()
+        {
+            return View(ListaCategoria.OrderBy(c => c.Name));
+        }
+
+
+        public ActionResult Edit(int Id)
+        {
+            var categorie = ListCategorie.Where(c => c.Id == Id).First();
+            return View(categorie);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Categorie categ)
+        {
+            var categorie = ListCategorie.Where(c => c.Id == categ.Id).First();
+            return RedirectToAction("Index");
+        }
+
+
+        public ActionResult Detalis(long Id)
+        {
+            var categoria = ListaCategoria.Where(c => c.Id == Id).First();
+            return View(categoria);
+        }
+
+        //GET
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Categorie categ)
+        {
+            ListaCategoria.Add(categ);
+            categ.Id = ListaCategoria.Select(c => c.Id).Max()+1;
+            return RedirectToAction("Index");
+        }
+
+        //GET
+
+        public ActionResult Delete(long id)
+        {
+            var categoria = ListaCategoria.Where(c => c.Id == id).First();
+            return View(categoria);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(Categorie categ)
+        {
+
+            var cat = ListaCategoria.Where(c => c.Id == categ.Id).First();
+            ListaCategoria.Remove(cat);
+            return RedirectToAction("Index");
+        }
+
+        private ActionResult VaiParaCategoriaView(long id)
+        {
+            var categoria = ListaCategoria.Where(c => c.Id == id).First();
+            return View(categoria);
+        }
+
+
+        private Categorie LerCategoria(long id)
+        {
+            return ListaCategoria.First(c => c.Id.Equals(id));
+        }
+
+
+    }
+}
---
+++ Controllers/SupplierController.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication2.Content;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    public class SupplierController : Controller
+    {
+
+        #region [ Propriedades ] 
+        private EFContexto contexto = new EFContexto(); 
+        #endregion
+
+
+
+        #region [ Actions ]
+        // GET: Fornecedor
+        public ActionResult Index()
+        {
+            var fornecedor = contexto.Fornecedores.OrderBy(s => s.Name);
+            return View(fornecedor);
+        }
+
+
+
+        //GET
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Supplier fornecedor)
+        {
+            contexto.Fornecedores.Add(fornecedor);
+            contexto.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        //GET
+        public ActionResult Edit(long? id)
+        {
+            if (id == null)
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            
+            var fornecedor = contexto.Fornecedores.Find(id);
+
+            if(fornecedor == null)
+                return HttpNotFound();
+
+            return View(fornecedor);
+
+
+        }
+
+
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Supplier fornecedor)
+        {
+            if (ModelState.IsValid)
+            {
+                contexto.Entry(fornecedor).State = EntityState.Modified;
+                contexto.SaveChanges();
+
+                return View(fornecedor);
+            }
+        }
+
+
+
+        #endregion
+
+
+
+
+
+    }
+}
